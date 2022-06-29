4d1c483fb115752af8a077f7b5d33b8d55c1daf7
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-06-29 19:55:06 +0200

    chore: add azure dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,19 +20,12 @@ repositories {
 }
 
 dependencies {
-    // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // This dependency is used by the application.
     implementation("com.google.guava:guava:30.1.1-jre")
-
-    // Use the Kotlin test library.
+    implementation("com.azure:azure-digitaltwins-core:1.2.3")
+    implementation("com.azure:azure-identity:1.5.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
---
+++ app/src/main/kotlin/swc/microservice/truck/usecases/TruckManager.kt
@@ -0,0 +1,2 @@
+package swc.microservice.truck.usecases
+
