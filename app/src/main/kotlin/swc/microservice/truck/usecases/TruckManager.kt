ea7bd22c72041757abe053a578bf1d2552ab77ee
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-06-29 20:44:01 +0200

    chore: start dealing with truck's digital twin

--- app/src/main/kotlin/swc/microservice/truck/usecases/TruckManager.kt
+++ app/src/main/kotlin/swc/microservice/truck/usecases/TruckManager.kt
@@ -1,2 +1,43 @@
 package swc.microservice.truck.usecases
 
+import com.azure.digitaltwins.core.DigitalTwinsClient
+import com.azure.digitaltwins.core.DigitalTwinsClientBuilder
+import com.azure.identity.AzureCliCredentialBuilder
+import swc.microservice.truck.entities.Truck
+import swc.microservice.truck.usecases.TruckDigitalTwinManager.Values.EMPTY_TRUCK_MODEL
+import swc.microservice.truck.usecases.TruckDigitalTwinManager.Values.MODEL_ID
+import java.io.File
+import java.nio.file.Files
+import java.util.stream.Collectors
+
+fun getAllTrucks(): List<Truck> = TODO()
+
+object TruckDigitalTwinManager {
+
+    object Values {
+        const val MODEL_ID = "dtmi:swc:Truck;1"
+        const val EMPTY_TRUCK_MODEL = "EmptyTruck.json"
+    }
+
+    private val client: DigitalTwinsClient = DigitalTwinsClientBuilder()
+        .credential(AzureCliCredentialBuilder().build())
+        .endpoint("https://test-instance.api.wcus.digitaltwins.azure.net/")
+        .buildClient()
+
+    private val twin: String = Files.readAllLines(
+        TruckDigitalTwinManager::class
+            .java
+            .getResource(EMPTY_TRUCK_MODEL)
+            ?.toURI()
+            ?.let { File(it) }
+            ?.toPath()
+    ).stream().collect(Collectors.joining())
+
+    fun createTruckDigitalTwin() {
+        val t = client.createOrReplaceDigitalTwin("0", twin, String.javaClass)
+    }
+
+    fun getTruckDigitalTwinModel() = client.getModel(MODEL_ID)
+
+
+}
