f86ab1bdd04fd983adbeda0cef7ae284fb3b18a3
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-07-01 17:30:54 +0200

    chore: update truck with default values and json serialization

--- app/src/main/kotlin/swc/microservice/truck/entities/Truck.kt
+++ app/src/main/kotlin/swc/microservice/truck/entities/Truck.kt
@@ -1,9 +1,14 @@
 package swc.microservice.truck.entities
 
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Klaxon
+
 data class Truck(
     val truckId: String,
-    val position: Position,
-    val occupiedVolume: Volume,
-    val capacity: Double,
+    val position: Position = Position(0L, 0L),
+    val occupiedVolume: Volume = Volume(0.0),
+    val capacity: Double = 100.0,
     val isInMission: Boolean = false
 )
+
+fun Truck.toJsonString(): String = Klaxon().toJsonObject(this).put("\$metadata", JsonObject(mapOf(Pair("\$model", this.truckId)))).toString()
