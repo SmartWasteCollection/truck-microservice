458359fbe4fcf1f2f01374f4d506da39869122d9
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-07-04 17:14:29 +0200

    chore: test delete digital twin

--- app/src/test/kotlin/swc/microservice/truck/TruckManagerTest.kt
+++ app/src/test/kotlin/swc/microservice/truck/TruckManagerTest.kt
@@ -1,13 +1,19 @@
 package swc.microservice.truck
 
+import com.azure.digitaltwins.core.implementation.models.ErrorResponseException
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import swc.microservice.truck.Values.ERROR_STATUS_CODE
 import swc.microservice.truck.Values.MODEL
+import swc.microservice.truck.Values.TRUCK_ID
 import swc.microservice.truck.entities.Truck
 import swc.microservice.truck.usecases.TruckManager
 
 object Values {
     const val MODEL = "Truck.json"
+    const val TRUCK_ID = "TruckTest"
+    const val ERROR_STATUS_CODE = 404
 }
 
 class TruckManagerTest : FreeSpec({
@@ -27,9 +33,16 @@ class TruckManagerTest : FreeSpec({
                 TruckManager.getTruckId() shouldBe "Truck${count}"
             }
             "should create and read a digital twin" {
-                val truck = Truck("Truck000")
+                val truck = Truck(TRUCK_ID)
                 TruckManager.createTruckDigitalTwin(truck)
                 TruckManager.readTruckDigitalTwin(truck.truckId) shouldBe truck
+            }
+            "should delete a digital twin" {
+                TruckManager.deleteTruckDigitalTwin(TRUCK_ID)
+                val e = shouldThrow<ErrorResponseException> {
+                    TruckManager.readTruckDigitalTwin(TRUCK_ID)
+                }
+                e.response.statusCode shouldBe ERROR_STATUS_CODE
             }
         }
     }
