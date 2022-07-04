84da56bfc7730d5ffd4698068b2e06bb1dd8cc08
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-07-04 17:59:38 +0200

    chore: add update methods for digital twins

--- app/src/main/kotlin/swc/microservice/truck/usecases/TruckSerialization.kt
+++ app/src/main/kotlin/swc/microservice/truck/usecases/TruckSerialization.kt
@@ -1,11 +1,13 @@
 package swc.microservice.truck.usecases
 
+import com.azure.core.models.JsonPatchDocument
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.google.gson.JsonSyntaxException
 import swc.microservice.truck.entities.Position
 import swc.microservice.truck.entities.Truck
 import swc.microservice.truck.entities.Volume
+import swc.microservice.truck.usecases.TruckSerialization.Serialization.toJsonString
 import swc.microservice.truck.usecases.TruckSerialization.Values.METADATA
 import swc.microservice.truck.usecases.TruckSerialization.Values.MODEL
 import swc.microservice.truck.usecases.TruckSerialization.Values.TRUCK_MODEL
@@ -35,60 +37,82 @@ object TruckSerialization {
         const val IN_MISSION = "inMission"
     }
 
-    /**
-     * Converts a [Truck] into a Json String.
-     */
-    fun Truck.toJsonString(): String = json (
-        Pair(METADATA, json(Pair(MODEL, quoted(TRUCK_MODEL)))),
-        Pair(POSITION, json(Pair(LATITUDE, this.position.latitude), Pair(LONGITUDE, this.position.longitude))),
-        Pair(OCCUPIED_VOLUME, json(Pair(VALUE, this.occupiedVolume.value))),
-        Pair(CAPACITY, this.capacity),
-        Pair(IN_MISSION, this.isInMission)
-    )
+    object Serialization {
 
-    /**
-     * Allows to convert a map of elements (represented by [Pair]s) into a Json String.
-     */
-    private fun <T> json(vararg pairs: Pair<String, T>): String = "{ ${
-        pairs.map { "\"${it.first}\": ${valueOf(it.second)}" }
-            .reduce { acc, s -> "$acc, $s" }
-    } }"
-
-    /**
-     * Converts a generic element into its corresponding Json representation value.
-     */
-    private fun <T> valueOf(elem: T): String = when (elem) {
-        is String -> elem
-        is Boolean -> when (elem) {
-            true -> "true"
-            else -> "false"
+        /**
+         * Produces a [JsonPatchDocument] that updates a serialized [Truck].
+         */
+        fun <T> patch(elem: T): JsonPatchDocument = when (elem) {
+            is Position -> JsonPatchDocument().appendReplace(POSITION,
+                json(Pair(LATITUDE, elem.latitude), Pair(LONGITUDE, elem.longitude)))
+            is Volume -> JsonPatchDocument().appendReplace(OCCUPIED_VOLUME,
+                json(Pair(VALUE, elem.value)))
+            is Boolean -> JsonPatchDocument().appendReplace(IN_MISSION, valueOf(elem))
+            else -> JsonPatchDocument()
         }
-        else -> elem.toString()
-    }
 
-    /**
-     * Quotes a string.
-     */
-    private fun quoted(s: String): String = "\"$s\""
-
-    /**
-     * Deserializes a Json into a Truck object.
-     */
-    fun deserialize(json: String): Truck {
-        val obj = json.obj()
-        return Truck(
-            truckId = obj[TRUCK_ID].asString,
-            position = Position(obj[POSITION].obj().get(LATITUDE).asLong, obj[POSITION].obj().get(LONGITUDE).asLong),
-            occupiedVolume = Volume(obj[OCCUPIED_VOLUME].obj().get(VALUE).asDouble),
-            capacity = obj[CAPACITY].asDouble,
-            isInMission = obj[IN_MISSION].asBoolean
+        /**
+         * Converts a [Truck] into a Json String.
+         */
+        fun Truck.toJsonString(): String = json(
+            Pair(METADATA, json(Pair(MODEL, quoted(TRUCK_MODEL)))),
+            Pair(POSITION, json(Pair(LATITUDE, this.position.latitude), Pair(LONGITUDE, this.position.longitude))),
+            Pair(OCCUPIED_VOLUME, json(Pair(VALUE, this.occupiedVolume.value))),
+            Pair(CAPACITY, this.capacity),
+            Pair(IN_MISSION, this.isInMission)
         )
+
+        /**
+         * Allows to convert a map of elements (represented by [Pair]s) into a Json String.
+         */
+        private fun <T> json(vararg pairs: Pair<String, T>): String = "{ ${
+            pairs.map { "\"${it.first}\": ${valueOf(it.second)}" }
+                .reduce { acc, s -> "$acc, $s" }
+        } }"
+
+        /**
+         * Converts a generic element into its corresponding Json representation value.
+         */
+        private fun <T> valueOf(elem: T): String = when (elem) {
+            is String -> elem
+            is Boolean -> when (elem) {
+                true -> "true"
+                else -> "false"
+            }
+            else -> elem.toString()
+        }
+
+        /**
+         * Quotes a string.
+         */
+        private fun quoted(s: String): String = "\"$s\""
+
     }
 
-    /**
-     * Converts [Any]thing into a [JsonObject]. Throws a [JsonSyntaxException] if the parsed object is not a valid json.
-     */
-    @kotlin.jvm.Throws(JsonSyntaxException::class)
-    private fun Any.obj(): JsonObject =
-        Gson().fromJson(this.toString(), JsonObject::class.java)
+    object Deserialization {
+
+        /**
+         * Deserializes a Json into a Truck object.
+         */
+        fun deserialize(json: String): Truck {
+            val obj = json.obj()
+            return Truck(
+                truckId = obj[TRUCK_ID].asString,
+                position = Position(
+                    obj[POSITION].obj().get(LATITUDE).asLong,
+                    obj[POSITION].obj().get(LONGITUDE).asLong
+                ),
+                occupiedVolume = Volume(obj[OCCUPIED_VOLUME].obj().get(VALUE).asDouble),
+                capacity = obj[CAPACITY].asDouble,
+                isInMission = obj[IN_MISSION].asBoolean
+            )
+        }
+
+        /**
+         * Converts [Any]thing into a [JsonObject]. Throws a [JsonSyntaxException] if the parsed object is not a valid json.
+         */
+        @kotlin.jvm.Throws(JsonSyntaxException::class)
+        private fun Any.obj(): JsonObject =
+            Gson().fromJson(this.toString(), JsonObject::class.java)
+    }
 }
