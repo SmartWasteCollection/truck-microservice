814d5d601ab2c628f65560ade4ad3e7ba8f9ddfa
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-07-04 17:29:09 +0200

    chore: wrap values in a private object

--- app/src/main/kotlin/swc/microservice/truck/usecases/TruckSerialization.kt
+++ app/src/main/kotlin/swc/microservice/truck/usecases/TruckSerialization.kt
@@ -2,24 +2,42 @@ package swc.microservice.truck.usecases
 
 import com.google.gson.Gson
 import com.google.gson.JsonObject
+import com.google.gson.JsonSyntaxException
 import swc.microservice.truck.entities.Position
 import swc.microservice.truck.entities.Truck
 import swc.microservice.truck.entities.Volume
+import swc.microservice.truck.usecases.TruckSerialization.Values.METADATA
+import swc.microservice.truck.usecases.TruckSerialization.Values.MODEL
+import swc.microservice.truck.usecases.TruckSerialization.Values.TRUCK_MODEL
+import swc.microservice.truck.usecases.TruckSerialization.Values.TRUCK_ID
+import swc.microservice.truck.usecases.TruckSerialization.Values.POSITION
+import swc.microservice.truck.usecases.TruckSerialization.Values.LATITUDE
+import swc.microservice.truck.usecases.TruckSerialization.Values.LONGITUDE
+import swc.microservice.truck.usecases.TruckSerialization.Values.OCCUPIED_VOLUME
+import swc.microservice.truck.usecases.TruckSerialization.Values.VALUE
+import swc.microservice.truck.usecases.TruckSerialization.Values.CAPACITY
+import swc.microservice.truck.usecases.TruckSerialization.Values.IN_MISSION
 
 object TruckSerialization {
 
-    private const val METADATA = "\$metadata"
-    private const val MODEL = "\$model"
-    private const val TRUCK_MODEL = "dtmi:swc:Truck;1"
-    private const val TRUCK_ID = "\$dtId"
-    private const val POSITION = "position"
-    private const val LATITUDE = "latitude"
-    private const val LONGITUDE = "longitude"
-    private const val OCCUPIED_VOLUME = "occupiedVolume"
-    private const val VALUE = "value"
-    private const val CAPACITY = "capacity"
-    private const val IN_MISSION = "inMission"
+    private object Values {
 
+        const val METADATA = "\$metadata"
+        const val MODEL = "\$model"
+        const val TRUCK_MODEL = "dtmi:swc:Truck;1"
+        const val TRUCK_ID = "\$dtId"
+        const val POSITION = "position"
+        const val LATITUDE = "latitude"
+        const val LONGITUDE = "longitude"
+        const val OCCUPIED_VOLUME = "occupiedVolume"
+        const val VALUE = "value"
+        const val CAPACITY = "capacity"
+        const val IN_MISSION = "inMission"
+    }
+
+    /**
+     * Converts a [Truck] into a Json String.
+     */
     fun Truck.toJsonString(): String = json (
         Pair(METADATA, json(Pair(MODEL, quoted(TRUCK_MODEL)))),
         Pair(POSITION, json(Pair(LATITUDE, this.position.latitude), Pair(LONGITUDE, this.position.longitude))),
@@ -28,12 +46,18 @@ object TruckSerialization {
         Pair(IN_MISSION, this.isInMission)
     )
 
-    private inline fun <reified T> json(vararg pairs: Pair<String, T>): String = "{ ${
-        pairs.map { "\"${it.first}\": ${reify(it.second)}" }
+    /**
+     * Allows to convert a map of elements (represented by [Pair]s) into a Json String.
+     */
+    private fun <T> json(vararg pairs: Pair<String, T>): String = "{ ${
+        pairs.map { "\"${it.first}\": ${valueOf(it.second)}" }
             .reduce { acc, s -> "$acc, $s" }
     } }"
 
-    private inline fun <reified T> reify(elem: T): String = when (elem) {
+    /**
+     * Converts a generic element into its corresponding Json representation value.
+     */
+    private fun <T> valueOf(elem: T): String = when (elem) {
         is String -> elem
         is Boolean -> when (elem) {
             true -> "true"
@@ -42,8 +66,14 @@ object TruckSerialization {
         else -> elem.toString()
     }
 
+    /**
+     * Quotes a string.
+     */
     private fun quoted(s: String): String = "\"$s\""
 
+    /**
+     * Deserializes a Json into a Truck object.
+     */
     fun deserialize(json: String): Truck {
         val obj = json.obj()
         return Truck(
@@ -55,5 +85,10 @@ object TruckSerialization {
         )
     }
 
-    private fun Any.obj(): JsonObject = Gson().fromJson(this.toString(), JsonObject::class.java)
+    /**
+     * Converts [Any]thing into a [JsonObject]. Throws a [JsonSyntaxException] if the parsed object is not a valid json.
+     */
+    @kotlin.jvm.Throws(JsonSyntaxException::class)
+    private fun Any.obj(): JsonObject =
+        Gson().fromJson(this.toString(), JsonObject::class.java)
 }
