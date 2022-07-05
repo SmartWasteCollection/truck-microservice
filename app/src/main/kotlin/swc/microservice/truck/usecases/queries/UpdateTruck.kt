1b95526485f5f49bcde99367ccee3b6d0dc3cece
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-07-05 11:59:03 +0200

    chore: add available trucks use case

--- app/src/main/kotlin/swc/microservice/truck/usecases/queries/UpdateTruck.kt
+++ app/src/main/kotlin/swc/microservice/truck/usecases/queries/UpdateTruck.kt
@@ -1,10 +1,17 @@
 package swc.microservice.truck.usecases.queries
 
-import swc.microservice.truck.entities.Position
+import swc.microservice.truck.entities.events.AvailabilityUpdateEvent
+import swc.microservice.truck.entities.events.OccupiedVolumeUpdateEvent
+import swc.microservice.truck.entities.events.PositionUpdateEvent
+import swc.microservice.truck.entities.events.TruckUpdateEvent
 import swc.microservice.truck.usecases.TruckManager
 
-class UpdateTruck<T>(private val truckId: String, private val newValue: T) : TruckQuery<Unit> {
+class UpdateTruck<T>(private val truckId: String, private val event: TruckUpdateEvent<T>) : TruckQuery<Unit> {
     override fun execute(manager: TruckManager) {
-        manager.updateTruck()
+        when (event) {
+            is PositionUpdateEvent -> manager.updateTruckPosition(truckId, event.newValue)
+            is OccupiedVolumeUpdateEvent -> manager.updateTruckOccupiedVolume(truckId, event.newValue)
+            is AvailabilityUpdateEvent -> manager.updateTruckInMission(truckId, event.newValue)
+        }
     }
 }
