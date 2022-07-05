61a633f27a2bbfda2ef5b03d460f9b153fa6e4bc
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-07-05 12:01:58 +0200

    chore: update usecases

--- app/src/main/kotlin/swc/microservice/truck/usecases/queries/GetAvailableTrucks.kt
+++ app/src/main/kotlin/swc/microservice/truck/usecases/queries/GetAvailableTrucks.kt
@@ -4,5 +4,5 @@ import swc.microservice.truck.entities.Truck
 import swc.microservice.truck.usecases.TruckManager
 
 class GetAvailableTrucks : TruckQuery<List<Truck>> {
-    override fun execute(manager: TruckManager): List<Truck> = manager.getAvailableTrucks()
+    override fun execute(manager: TruckManager): List<Truck> = manager.getAllTrucks().filter { !it.isInMission }
 }
