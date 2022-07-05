799b16d2d37e83cece45c417f216af091d2b754b
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-07-05 13:37:59 +0200

    chore: query all trucks

--- app/src/main/kotlin/swc/microservice/truck/drivers/TruckDigitalTwinManager.kt
+++ app/src/main/kotlin/swc/microservice/truck/drivers/TruckDigitalTwinManager.kt
@@ -3,25 +3,21 @@ package swc.microservice.truck.drivers
 import com.azure.digitaltwins.core.DigitalTwinsClient
 import com.azure.digitaltwins.core.DigitalTwinsClientBuilder
 import com.azure.identity.AzureCliCredentialBuilder
-import swc.microservice.truck.entities.Position
-import swc.microservice.truck.entities.Truck
-import swc.microservice.truck.entities.Volume
-import swc.microservice.truck.adapters.TruckPresentation.Serialization.toJsonString
-import swc.microservice.truck.drivers.TruckDigitalTwinManager.Values.DT_QUERY
-import swc.microservice.truck.drivers.TruckDigitalTwinManager.Values.ENDPOINT
-import swc.microservice.truck.drivers.TruckDigitalTwinManager.Values.MODEL_ID
 import swc.microservice.truck.adapters.TruckPresentation.Deserialization.deserialize
 import swc.microservice.truck.adapters.TruckPresentation.Deserialization.toJsonObject
 import swc.microservice.truck.adapters.TruckPresentation.Serialization.patch
+import swc.microservice.truck.adapters.TruckPresentation.Serialization.toJsonString
+import swc.microservice.truck.drivers.DigitalTwinsValues.DIGITAL_TWIN_MODEL
+import swc.microservice.truck.drivers.DigitalTwinsValues.ENDPOINT
+import swc.microservice.truck.drivers.DigitalTwinsValues.TRUCK_COUNT
+import swc.microservice.truck.drivers.DigitalTwinsValues.TRUCK_MODEL_ID
+import swc.microservice.truck.drivers.QueryBuilder.QueryElements.EQUALS
+import swc.microservice.truck.entities.Position
+import swc.microservice.truck.entities.Truck
+import swc.microservice.truck.entities.Volume
 import swc.microservice.truck.usecases.TruckManager
 
 class TruckDigitalTwinManager : TruckManager {
-
-    object Values {
-        const val MODEL_ID: String = "dtmi:swc:Truck;1"
-        const val ENDPOINT: String = "https://test-instance.api.wcus.digitaltwins.azure.net/"
-        const val DT_QUERY: String = "SELECT COUNT() FROM digitaltwins WHERE \$metadata.\$model = 'dtmi:swc:Truck;1'"
-    }
 
     private val client: DigitalTwinsClient = DigitalTwinsClientBuilder()
         .credential(AzureCliCredentialBuilder().build())
@@ -31,10 +27,14 @@ class TruckDigitalTwinManager : TruckManager {
     /**
      * Counts how many [Truck] digital twin are deployed.
      */
-    override fun getTruckCount(): Int =
-        client.query(DT_QUERY, String::class.java)
-            .map { it.toString().toJsonObject() }[0]["COUNT"].asInt
-
+    override fun getTruckCount(): Int {
+        val query = QueryBuilder()
+            .selectCount()
+            .where(DIGITAL_TWIN_MODEL, EQUALS, "'$TRUCK_MODEL_ID'")
+            .build()
+        return client.query(query, String::class.java)
+            .map { it.toString().toJsonObject() }[0][TRUCK_COUNT].asInt
+    }
 
     /**
      * Creates a digital twin of the specified [Truck].
@@ -77,14 +77,19 @@ class TruckDigitalTwinManager : TruckManager {
      * Gets all the deployed [Truck]s in the digital twin instance.
      */
     override fun getAllTrucks(): List<Truck> {
-        TODO("Not yet implemented")
+        val query = QueryBuilder()
+            .selectAll()
+            .where(DIGITAL_TWIN_MODEL, EQUALS, "'$TRUCK_MODEL_ID'")
+            .build()
+        return client.query(query, String::class.java)
+            .map { deserialize(it.toJsonObject().toString()) }
     }
 
     /**
      * Gets the digital twin model of a [Truck].
      */
     fun getTruckDigitalTwinModel(): String =
-        client.getModel(MODEL_ID).dtdlModel
+        client.getModel(TRUCK_MODEL_ID).dtdlModel
 
 }
 
