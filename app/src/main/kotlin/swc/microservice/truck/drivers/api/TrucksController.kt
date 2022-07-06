befdc7ef2cb9cb9f0454cb1b8b4ab331e14a9312
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-07-06 13:53:03 +0200

    chore: add get and post routes

--- app/src/main/kotlin/swc/microservice/truck/drivers/api/TrucksController.kt
+++ app/src/main/kotlin/swc/microservice/truck/drivers/api/TrucksController.kt
@@ -2,11 +2,18 @@ package swc.microservice.truck.drivers.api
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import swc.microservice.truck.adapters.TruckPresentation.Deserialization.deserialize
 import swc.microservice.truck.entities.Truck
+import swc.microservice.truck.usecases.CreateTruck
+import swc.microservice.truck.usecases.GetAvailableTrucks
 import swc.microservice.truck.usecases.GetTruck
+import swc.microservice.truck.usecases.TruckCount
 import swc.microservice.truck.usecases.TruckManager
+import swc.microservice.truck.usecases.TruckNextId
 
 @RestController
 @RequestMapping("/trucks")
@@ -14,4 +21,13 @@ class TrucksController(val manager: TruckManager = ManagerSupplier.get()) {
 
     @GetMapping("/{id}")
     fun getTruck(@PathVariable id: String): Truck? = GetTruck(id).execute(manager)
+
+    @GetMapping("/available")
+    fun getAvailableTrucks(): List<Truck> = GetAvailableTrucks().execute(manager)
+
+    @GetMapping("/count")
+    fun getTruckCount(): Int = TruckCount().execute(manager)
+
+    @PostMapping("/")
+    fun createTruck(@RequestBody body: String) = CreateTruck(deserialize(body, TruckNextId().execute(manager))).execute(manager)
 }
