6ddad55556fbfc68c3a3ba95c30631ff5e78d543
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-07-08 17:14:57 +0200

    fix: set server port from environment variable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("com.google.guava:guava:31.1-jre")
     implementation("com.azure:azure-digitaltwins-core:1.2.3")
     implementation("com.azure:azure-identity:1.5.2")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.3.1")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
--- app/src/main/kotlin/swc/microservice/truck/TruckMicroservice.kt
+++ app/src/main/kotlin/swc/microservice/truck/TruckMicroservice.kt
@@ -1,11 +1,19 @@
 package swc.microservice.truck
 
+import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 open class TruckMicroservice
 
-fun main(args: Array<String>) {
-    runApplication<TruckMicroservice>(*args)
+const val PORT_KEY = "server.port"
+const val DEFAULT_PORT = "3001"
+
+fun main() {
+    val dotenv = dotenv {
+        ignoreIfMissing = true
+    }
+    System.setProperty(PORT_KEY, dotenv[PORT_KEY] ?: DEFAULT_PORT)
+    runApplication<TruckMicroservice>()
 }
